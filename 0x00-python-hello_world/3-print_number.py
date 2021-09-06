476c658c8e239552c8dd11756158ac28dda2f639
wuhibe <wuhibetamire710@gmail.com>
2021-09-06 19:05:26 +0300

    All Py

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 98
-print(number, "Battery Percent")
+print("{:.0f} Battery Percent".format(number))
---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:-53]+str[107:-17]+str[0:-123]
+print(str)
