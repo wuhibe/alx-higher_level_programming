5a323aee8230042a9bfeeafbebc178ff02177775
wuhibe <wuhibetamire710@gmail.com>
2021-09-12 20:43:30 +0300

    Format Outpu

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list):
-    my_list.reverse()
+    if len(my_list) > 1:
+        my_list.reverse()
     for i in my_list:
-        print("{}".format(i))
+        print("{:d}".format(i))
