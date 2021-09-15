e72cc02a7a0eb27535bd05529118a8ff41f4e5a6
wuhibe <wuhibetamire710@gmail.com>
2021-09-15 22:02:50 +0300

    Average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list:
+        average = 0
+        denom = 0
+        summ = 0
+        for i in my_list:
+            denom += i[1]
+            summ += i[0] * i[1]
+        return summ/denom
