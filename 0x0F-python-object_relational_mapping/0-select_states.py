062d4082850a7ccd58a0aec73bf95f7e538c4e34
wuhibe <wuhibetamire710@gmail.com>
2021-12-18 19:31:56 +0300

    pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
+def select_states():
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
 
-db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states")
-states = cur.fetchall()
-for state in states:
-    print(state)
+
+if __name__ == "__main__":
+    select_states()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
+def filter_state():
+    import sys
+    import MySQLdb
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{:s}'".format(sys.argv[4]))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
 
-db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name = '{:s}'".format(sys.argv[4]))
-states = cur.fetchall()
-for state in states:
-    print(state)
+
+if __name__ == "__main__":
+    filter_state()
