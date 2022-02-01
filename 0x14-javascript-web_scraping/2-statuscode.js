8dae3193a6298eec1d62d929038137a443121f74
wuhibe <wuhibetamire710@gmail.com>
2022-02-01 17:41:25 +0300

    statuscode

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
