140356afd4fdb33d20a72643089f76d83877c7df
wuhibe <wuhibetamire710@gmail.com>
2022-02-01 21:45:21 +0300

    count 18's

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+let count = 0;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const abc = JSON.parse(body);
+    abc.results.forEach(function (item, index, array) {
+      array = item.characters;
+      array.forEach(function (abc) {
+        if (abc.includes('18')) {
+          count = count + 1;
+        }
+      });
+    });
+    console.log(count);
+  }
+});
