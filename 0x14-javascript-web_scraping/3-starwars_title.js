1864b4370bbc9818a9a785dcc1689d6e87470b7b
wuhibe <wuhibetamire710@gmail.com>
2022-02-01 20:58:35 +0300

    JSON parse

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    console.log(body.title);
+    let abc = JSON.parse(body);
+    console.log(abc.title);
   }
 });
